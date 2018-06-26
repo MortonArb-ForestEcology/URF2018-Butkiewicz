7c10926b1cd2ef754c44ce988c1f46ac682309fc
Cori Butkiewicz <cori.butkiewicz@gmail.com>
2018-06-26 09:11:45 -0500

    Getting Climate Variable

---
+++ Met_Data_Variable.R
@@ -0,0 +1,12 @@
+PDSI.GLSP <- read.csv("./PDSI_AllMembers.csv") #Reading in the CSV, and called it PDSI (for the name of the drought index) .GLSP (for Glacial Lakes state park).
+# Each column is a different weather. 
+
+# Add columns that describe the year, month, and day. Row 1=January 1 1800. 
+
+# Subset the table for only 1800-1829. 
+
+# Subset the table for June, July, and August. 
+
+# Then find the column that has the highest average value for June, July and August.
+
+# Find the column that has the minimum average value for June, July and August. 
