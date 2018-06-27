63c846029bf55c51a5792b642b4e256034ca55cb
Cori Butkiewicz <cori.butkiewicz@gmail.com>
2018-06-27 09:47:56 -0500

    A Sloppy Version of the Met Data
    I decided to commit the sloppy code. I plan on cleaning it up later, but this is just to make sure that everything was done right. Nothing about this code is efficient.

--- Met_Data_Variable.R
+++ Met_Data_Variable.R
@@ -32,22 +32,43 @@ PDSI.GLSP_29yr_JJA <- subset(PDSI.GLSP_29yr_JJA,subset=month<=8)
 PDSI.GLSP_29yr_JJA$month
 max(PDSI.GLSP_29yr_JJA$month)
 
-# Then average June, July and August. 
+# Then average June, July and August to get the average PDSI values during the growing season.  The next chunk of code is super sloppy so I'll just stick lots of comments on here.
+
 y=1800
-averages <- NULL
+averages <- NULL #I created essentially an empty dataframe to put things in. 
 while(y<=1829){
-  print(y) 
-  tempdf <- PDSI.GLSP_29yr_JJA[which(PDSI.GLSP_29yr_JJA$year==y),]
-  averages <- rbind(averages,colMeans(tempdf))
+  print(y) #This is just here to make sure that it's iterating through the years. 
+  tempdf <- PDSI.GLSP_29yr_JJA[which(PDSI.GLSP_29yr_JJA$year==y),] #A temporary dataframe that gets overwritten constantly. By the end of this loop it should only have 3 rows and 203 columns. 
+  averages <- rbind(averages,colMeans(tempdf)) 
   y=y+1
 }
-summary(averages)
+summary(averages) #A check to make sure this did what I wanted it to do. 
 averages <- data.frame(averages)
-
-#Average the columns across each year.
-averages_yr <-data.frame(colMeans(averages))
-averages_yr.test <- averages_yr
-averages_yr <- averages_yr[-c(202,203),]
+nrow(averages) #Since there's only 30 years in the dataset there should only be 30 rows in averages, one for each year. 
 
 
-# Find the column that has the minimum average value for June, July and August. 
+#Average the columns across the years to get the total average PDSI during the growing season from 1800-1829 for each weather. Right now everything is stored in a new variable. If I get a chance to clean this up the variables will be overwritten, but for now this works too. 
+
+averages_yr <-data.frame(colMeans(averages)) #I took the averages of all the columns in averages. 
+rownames(averages_yr)
+averages_yr.PDSI <- averages_yr[-c(1,202,203),] #A bit of soft-coding here, I knew the row numbers I wanted to delete. This should delete the columns labeled "X", "month", and "year".
+averages_yr.rownames <- rownames(averages_yr) #I couldn't figure a way to get a data frame with what I had, so I made two vectors, one with the desired columns and one with the desired rows. 
+averages_yr.rownames #Just to make sure that everything's here. 
+averages_yr.rownames <- averages_yr.rownames[-c(1,202,203)] #Subset the vector. 
+averages_yr.rownames #Check again. 
+averages_yr.test <- data.frame(weather=averages_yr.rownames,PDSI=averages_yr.PDSI) 
+averages_yr.test
+
+#Find the column with the maximum average PDSI for June, July, and August--the wettest weather. 
+
+rownames(averages_yr.test) <- averages_yr.rownames
+averages_yr.test #Should give you a column labeled "weather" and a column labeled "PDSI"
+max(averages_yr.test$PDSI)
+max_average_weather <- subset(averages_yr.test,subset=PDSI==max(averages_yr.test$PDSI))
+max_average_weather #Should print the maximum average PDSI value found in the table and the weather associated with it. 
+
+# Find the column that has the minimum average value for June, July and August--the driest weather.
+
+min(averages_yr.test$PDSI)
+min_average_weather <- subset(averages_yr.test,subset=PDSI==min(averages_yr.test$PDSI))
+min_average_weather #Should print the minimum average PDSI value found in the table and the weather associated with it. 
