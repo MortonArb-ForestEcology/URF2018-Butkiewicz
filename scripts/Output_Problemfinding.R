941c2d683b691e855ff7e7d373f8b75fb030bc72
Cori Butkiewicz <cori.butkiewicz@gmail.com>
2018-07-09 14:10:38 -0500

    Added Checks

--- scripts/Output_Problemfinding.R
+++ scripts/Output_Problemfinding.R
@@ -2,4 +2,13 @@
 # Investigating the Output files of our initial Spin-Ups # 
 ##########################################################
 
-path.output <- ("../ED_Workflow/1_spin_initial/extracted_ouput")
+# The path scripts will	be run so that:                	
+###   .ds = dry, sandy
+###   .dc = dry, clay-y
+###   .ws = wet, sandy
+###   .wc = wet, clay-y
+path.ds <- ("../ED_Workflow/1_spin_initial/extracted_ouput/CD-SS-FN-TN-IN")
+files.ds <- dir(path.ds)
+class(files.ds)
+length(files.ds)
+print(files.ds)
