bc88e9dfa3fba1f14576350a4a27e8d77b2fed42
Cori Butkiewicz <cori.butkiewicz@gmail.com>
2018-07-09 13:53:49 -0500

    New File

---
+++ scripts/Output_Problemfinding.R
@@ -0,0 +1,5 @@
+##########################################################
+# Investigating the Output files of our initial Spin-Ups # 
+##########################################################
+
+path.output <- ("../ED_Workflow/1_spin_initial/extracted_ouput")
