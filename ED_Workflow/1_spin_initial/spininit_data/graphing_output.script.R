8d3429849c9035b329c4ccc860d58429db14c7b6
Cori Butkiewicz <cori.butkiewicz@gmail.com>
2018-07-12 13:03:31 -0500

    Graph for 1801 Finished
    Warm-up in which the above-ground biomass is graphed. Next step is to loop through an entire run.

--- ED_Workflow/1_spin_initial/spininit_data/graphing_output.script.R
+++ ED_Workflow/1_spin_initial/spininit_data/graphing_output.script.R
@@ -1,8 +1,9 @@
-##############
-# AGB Graphs #
-##############
+###############################
+# Above Ground Biomass Graphs #
+###############################
 
 library(ncdf4)
+library(modeest)
 
 #The goal of this code is to produce a graph (or two) that compares the AGB of different plant functional types. 
 #First we'll be working with one file, then we'll be working with multiple, until finally we can produce something with the entire server.
@@ -16,14 +17,47 @@ library(ncdf4)
 path.nc <- "/Users/Cori/Research/Morton_Arb/Project/Die/"
 files.nc <- dir(path.nc) #This will have to change, since I'll need to exclude the nc.var files somehow. I have some ideas, but I don't know if it'll work. But for now, this will suffice. 
 
-test.agb <- nc_open(file.path(path.nc,files.nc[1]))
-Table.agb <- data.frame(ncvar_get(test.agb,"Cohort_AbvGrndBiom"))
-Table.pft <- data.frame(ncvar_get(test.agb,"Cohort_PFT"))
-Table.pft_name <- data.frame(ncvar_get(test.agb,"PFT_name"))
+test.nc <- nc_open(file.path(path.nc,files.nc[1]))
+Table.agb <- data.frame(ncvar_get(test.nc,"Cohort_AbvGrndBiom"))
+Table.pft <- data.frame(ncvar_get(test.nc,"Cohort_PFT")) #5 means it's a C3 grass, 10 means it's a mid-successional hardwood (an oak). 0 means there's no biomass at the site and the cohort does not exist, either yet or anymore. 
+Table.pft_name <- data.frame(ncvar_get(test.nc,"PFT_name"))
+nc_close(test.nc)
 
-#Okay. So I have a table with the aboveground biomass, and a table with the cohort PFTs. So I need to find some way to combine the cohorts a bit--that means I'm going to have to combine some rows--keep the columns separate. 
+#Okay. So I have a table with the aboveground biomass, and a table with the cohort PFTs. So I need to find some way to combine the cohorts a bit--that means I'm going to have to combine some rows--keep the columns separate. I need to find a way to make sure that each cohort and each month remains associated with each other. 
 
-?aggregate
+Table.pft[Table.pft==0] <- NA #Replaces the 0's with NA to make the data easier to manage. 
+
+for(i in 1:nrow(Table.pft)){
+  if(i==1){
+    temp_vector <- as.numeric(Table.pft[i,])
+    print(class(temp_vector))
+    x <- mfv(temp_vector)
+    modes <- x
+    print(modes)
+  } else {
+    temp_df <- subset(Table.pft[i,])
+    temp_vector <- as.numeric(Table.pft[i,])
+    x <- mfv(temp_vector)
+    modes <- append(modes,values=x)
+    print(modes)
+  }
+}
+Table.pft$modes <- modes #This dataframe should now have the modes at the end of the rows.
+
+agb.trees <- Table.agb[Table.pft$modes==10, ]
+agb.trees <-data.frame(agb=colSums(agb.trees))
+agb.trees$pft <- "Hardwoods"
+agb.grasses <- Table.agb[Table.pft$modes==5, ]
+agb.grasses <- data.frame(agb=colSums(agb.grasses))
+agb.grasses$pft <- "Grasses"
+
+agb.data <- rbind(agb.grasses,agb.trees)
+
+days <- c(test.nc$var$Cohort_AbvGrndBiom$dim[[4]]$vals)
+agb.data$days <- days
+
+#Cleanup
+rm(agb.trees,agb.grasses,temp_df,temp_vector,modes,days)
 
 # -------------------------- #
 # Graphing the extracted AGB #
@@ -31,5 +65,8 @@ Table.pft_name <- data.frame(ncvar_get(test.agb,"PFT_name"))
 
 library(ggplot2)
 
-#This will probably be moved to a separate code so that I can use one on the server and one on my laptop. It'll be fun. I hope. 
+plot <- ggplot(agb.data,aes(x=days,y=agb,color=pft))+geom_line()+xlab("Days since 01-01-1801")+ylab("Above Ground Biomass, kg C m-2")
+plot + scale_color_manual(values=c("#E69F00","#009E73"))
+
+#This will probably be moved to a separate code so that I can use one on the server and one on my laptop. It'll be fun. I hope. But for now it'll be here so that I can see it.  
 
