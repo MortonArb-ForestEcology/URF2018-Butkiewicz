febf33abe2769c977d8e53f6aad418fd0e001607
Cori Butkiewicz <cori.butkiewicz@gmail.com>
2018-07-16 13:01:58 -0500

    Bug Fixes Not Working

--- ED_Workflow/1_spin_initial/spininit_data/graph_script.R
+++ ED_Workflow/1_spin_initial/spininit_data/graph_script.R
@@ -8,10 +8,13 @@ if(!dir.exists("./graphs/")) dir.create("./graphs/", recursive = T) #This should
 path.data <- ("./tables/tables/")
 all.data <- dir(path.data)
 
+i=1
 for(RUNID in all.data){
   print(RUNID)
-  agb.data <- read.csv(file.path(path.data,all.data[RUNID])) #Should build a filepath where I can read each .csv in the list. But it's not working, and I don't know why. It just keeps telling me that there's no such file or directory.
+  file_name <- c(path.data,all.data[RUNID]) #all.data[i] is not subsetting the list the way I need ti tot subset the list. 
+  agb.data <- read.csv(file_name) #Should build a filepath where I can read each .csv in the list. But it's not working, and I don't know why. It just keeps telling me that there's no such file or directory.
   jpeg("../graphs",RUNID,"_graph")
   ggplot(agb.data,aes(x=days,y=agb,color=pft))+geom_line()+xlab("Days since 01-01-1801")+ylab("Above Ground Biomass, kg C m-2")+theme(panel.background=element_rect(fill="white"))+theme_bw()+theme(panel.grid=element_blank())+scale_color_manual(values=c("#E69F00","#009E73"))
   dev.off()
 }
+
