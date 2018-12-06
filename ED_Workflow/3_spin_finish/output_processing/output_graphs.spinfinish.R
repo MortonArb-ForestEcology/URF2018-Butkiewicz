0d0a26163f299d7b359184da4fac9cdf5b69333d
Cori Butkiewicz <cori.butkiewicz@gmail.com>
2018-12-06 10:33:01 -0500

    New graphs

--- ED_Workflow/3_spin_finish/output_processing/output_graphs.spinfinish.R
+++ ED_Workflow/3_spin_finish/output_processing/output_graphs.spinfinish.R
@@ -5,349 +5,84 @@
 # This is not meant to be run on the GitHub repository, but instead provides an example of how I generated the graphs in my results.
 
 library(ggplot2)
+library(car)
+
+# Get the data into the environment: 
 # files.output <- file.path("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/")
 FRI <- read.csv("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/output_FRI_v4.csv")
-my_output <- read.csv("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/output_runs_v4.csv") #Reads in dataframe with pft, agb, density, dbh, basal area, etc. 
-my_output <- merge(my_output, FRI)
+FRI$FRI <- car::recode(FRI$FRI, "'Inf'='100'")
+dat.all <- read.csv("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/output_runs_v4.csv") #Reads in dataframe with pft, agb, density, dbh, basal area, etc. 
 
-# Edit the datatable
-RUNID <- as.character(my_output$RUNID)
+# Modify the data in whatever way I think will make graphing easier:
+dat.all <- merge(dat.all, FRI)
+RUNID <- as.character(dat.all$RUNID) 
 factors <- t(data.frame(strsplit(RUNID, split="-f")))
 rownames(factors) <- c() # Gets rid of the row names that show up.
 colnames(factors) <- c("Soil","Fire") # Generate more user-friendly column names. 
 factors <- data.frame(factors)
-my_output <- cbind(factors,my_output)
-my_output$SLXSAND <- car::recode(my_output$Soil, "'s1'='0.93'; 's2'='0.8'; 's3'='0.66'; 's4'='0.52'; 's5'='0.38'")
-my_output$SM_FIRE <- car::recode(my_output$Fire, "'1'='0.04'; '2'='0.03'; '3'='0.02'; '4'='0.01'; '5'='0'")
-# my_output <- my_output[,-2]
+dat.all <- cbind(factors,dat.all) # Allows me to sort data based on soil or fire, instead of soil and fire combinations. 
+dat.all$SLXSAND <- car::recode(dat.all$Soil, "'s1'='0.93'; 's2'='0.8'; 's3'='0.66'; 's4'='0.52'; 's5'='0.38'")
+dat.all$SM_FIRE <- car::recode(dat.all$Fire, "'1'='0.04'; '2'='0.03'; '3'='0.02'; '4'='0.01'; '5'='0'")
 
-# my_output.ba <- read.csv("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/output_batables_v4.csv")
-# my_output.ba <- merge(my_output.ba,FRI)
-# my_output.ba <- cbind(factors,my_output.ba)
-# my_output.ba$Soil <- car::recode(my_output.ba$Soil, "'s1'='0.93'; 's2'='0.8'; 's3'='0.66'; 's4'='0.52'; 's5'='0.38'")
-# my_output.ba <- my_output.ba[,-2]
-# 
-# # my_output_subset <- subset(my_output, subset=year>=410) #Subset last ninety years so that we can look at the stability of the ecosystems.
-# summary(my_output)
+# Aggregate data down all 101 years to make 
+dat.all.agg <- aggregate(x=dat.all[,c("p.dens","p.dens.tree","p.ba","soil_moist","p.agb")],
+                         by=dat.all[,c("RUNID","SLXSAND","SM_FIRE")],
+                         FUN=mean)
+dat.all.agg <- merge(dat.all.agg, FRI)
 
-# Gives a theme for this particular graph. 
-mytheme <- theme_bw()+
-  theme(plot.title = element_text(hjust = 0.5),
-        panel.background=element_rect(fill="white"),
-        panel.grid=element_blank(),
-        text=element_text(family="Helvetica"))
+#------------------------------
+# STANDARDIZED SOIL GRAPHS
+### Lines 34-_____ create a set of standardized soil moisture values and a graph. Because the soil moisture varies so much with soil texture, determining whether it depends on the fire frequency requires that the values be compared against our control scenario: s5, which had fire turned off. 
+#------------------------------
 
-# ---------------------------------
-# CHANGE IN SOIL MOISTURE OVER TIME
-# ---------------------------------
+soil <- c(0.38, 0.52, 0.66, 0.8, 0.93)
+sm.calc <- matrix(0, ncol=5, nrow=length(soil)) #make a matrix to put things into where the rows will correspond to the soil
+for(s in 1:length(soil)){
+  temp <- dat.all.agg[which(dat.all.agg$SLXSAND==soil[s]),] #make temporary vector with soil moisture
+  temp <- temp$soil_moist #get rid of unnecessary columns
+  sm.calc[s,] <- temp #fill soil moisture values into row
+}
+data.frame(sm.calc)
+rownames(sm.calc) <- soil
+colnames(sm.calc) <- c("f5","f4","f3","f2","f1")
 
-pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/soil_moisture_all.pdf")
-ggplot(my_output,aes(x = year, y = soil_moist)) +
+sm.standard <- matrix(0, ncol=4, nrow=length(soil)) #make another matrix to put the standardized values into
+rownames(sm.standard) <- soil
+for(i in 2:ncol(sm.calc)){
+  sm.standard[,i-1] <- sm.calc[,i] - sm.calc[,5]
+} #This loop might be just a little messed up. I need to lay with it some more. 
+# rm(sm.calc)
+sm.standard <- data.frame(sm.standard) #make it into a dataframe
+colnames(sm.standard) <- c("0.04","0.03","0.02","0.01") #change the colnames to something reasonable
+sm.standard <- stack(sm.standard) #stack them so that it's easier to graph
+sm.standard$SLXSAND <- rep(soil) #
+colnames(sm.standard) <- c("soilmoist","SM_FIRE","SLXSAND") #
+RUNID.2 <- dat.all.agg[,1:3]
+sm.standard <- merge(RUNID.2, sm.standard)
+sm.standard <- merge(sm.standard,FRI)
+
+# --------
+# OTHER GRAPHS
+# ---------
+
+# Standardized Soil Moisture vs. FRI
+# pdf("/Users/Cori/Desktop/graph")
+ggplot(sm.standard, aes(x=FRI, y=soilmoist)) + #Compares soil moisture against fire return interval to see if there's any covariation. 
+  geom_point() + 
+  facet_grid(. ~ SLXSAND) #Labels on the top represent proportion of sand in the soil. 
+# dev.off()
+
+# Density vs. Soil Moisture
+ggplot(dat.all.agg, aes(x=SM_FIRE, y=p.dens.tree, color=SLXSAND)) + 
+  geom_point()
+
+# Density vs. FRI
+ggplot(dat.all.agg, aes(x=as.numeric(paste(SLXSAND)), y=p.dens.tree, color=SM_FIRE)) + 
   geom_line() +
-  xlab("Years") +
-  ylab("Soil Moisture") + 
-  facet_grid(SLXSAND ~ SM_FIRE) + 
-  # geom_vline(aes(xintercept = which(my_output$fire="Yes"))) +
-  ggtitle("Soil Moisture") +
-  mytheme
-dev.off()
+  geom_point()
 
-# ----------------------------
+ggplot(dat.all, aes(x=SLXSAND, y=p.dens.tree, fill=SM_FIRE)) + 
+  geom_boxplot() 
 
-# ----------------------------
-# CHANGE IN DENSITY OVER TIME
-# ----------------------------
-pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/density.pdf")
-ggplot(my_output,aes(x = year, y = p.dens.tree)) +
-  geom_line(color='dark green') +
-  xlab("Years") +
-  ylab("Density") + 
-  facet_grid(SLXSAND~SM_FIRE) + 
-  # geom_vline(aes(xintercept = which(my_output$fire="Yes"))) +
-  ggtitle("Tree Density (>10 cm DBH)") +
-  mytheme
-dev.off()
-
-ggplot(my_output,aes(x = year, y = p.dens.tree)) +
-  geom_line(color='dark green') +
-  xlab("Years") +
-  ylab("Density") + 
-  facet_grid(SM_FIRE~SLXSAND) + 
-  # geom_vline(aes(xintercept = which(my_output$fire="Yes"))) +
-  ggtitle("Tree Density (>10 cm DBH)") +
-  mytheme
-
-# --------------------------------
-# CHANGE IN BASAL AREA OVER TIME
-# --------------------------------
-pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/basal_area.pdf")
-ggplot(my_output,aes(x = year, y = p.ba)) +
-  geom_line(color='brown') +
-  xlab("Years") +
-  ylab("Basal Area, cm^2") + 
-  facet_grid(SLXSAND~SM_FIRE) + 
-  # geom_vline(aes(xintercept = which(my_output$fire="Yes"))) +
-  ggtitle("Basal Area") +
-  mytheme
-dev.off()
-
-# ----------------------------
-
-# -------------------------------
-# CHANGE IN DBH OVER TIME
-# -------------------------------
-
-pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/dbh.pdf")
-ggplot(my_output,aes(x = year, y = p.dbh)) +
-  geom_line(color='blue') +
-  xlab("Years") +
-  ylab("DBH, cm") + 
-  facet_grid(SLXSAND~SM_FIRE) + 
-  # geom_vline(aes(xintercept = which(my_output$fire="Yes"))) +
-  ggtitle("DBH of all Trees") +
-  mytheme
-dev.off()
-
-pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/dbh_tree.pdf")
-ggplot(my_output,aes(x = year, y = p.dbh.tree)) +
-  geom_line(color='dark blue') +
-  xlab("Years") +
-  ylab("DBH, cm") + 
-  facet_grid(SLXSAND~SM_FIRE) + 
-  # geom_vline(aes(xintercept = which(my_output$fire="Yes"))) +
-  ggtitle("DBH of trees >10 cm") +
-  mytheme
-dev.off
-
-# --------------------------
-
-# Interesting to look at the table in this way: 
-my_output.test <- subset(my_output, subset=my_output$year==101)
-test <- aggregate(my_output.test$p.dbh, by=list(my_output.test[,"FRI"]), FUN=mean)
-colnames(test) <- c("FRI","dbh")
-temp <- aggregate(my_output.test$p.dens.tree, by=list(my_output.test[,"FRI"]), FUN=mean)
-test$p.dens.tree <- temp[,2]
-temp <- aggregate(my_output.test$p.ba, by=list(my_output.test[,"FRI"]), FUN=mean)
-test$p.ba <- temp[,2]
-
-pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/FRI_scatter")
-ggplot(test, aes(x=FRI, y=p.dens.tree)) + 
-  geom_point(color="dark orange") +
-  xlab("Fire Return Inverval") + 
-  ylab("Tree Density") + 
-  mytheme
-dev.off
-
-test.cub <- test[1:9,]
-ggplot(test.cub, aes(x=FRI, y=dens.tree)) + 
-  geom_point() +
-  xlab("Fire Return Inverval") + 
-  ylab("Tree Density") + 
-  mytheme
-
-# ----------
-# DOES DENSITY CORRELATE WITH SOIL MOISTURE?
-# ----------
-
-pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/SM_scatter")
-ggplot(my_output, aes(x=soil_moist, y=p.dens.tree)) + 
-  geom_point() +
-  xlab("Soil Moisture") + 
-  ylab("Tree Density") + 
-  mytheme
-dev.off()
-
-# # ---------------------------------------------------
-# 
-# # GRAPH OF CHANGE IN FRACTIONAL TREE COVER OVER TIME
-# # ---------------------------------------------------
-# # Basal area was calculated from dbh, tree density, and patch area, and then tracked over time. The graph is labeled with green, 
-# # orange, and yellow regions. If the line falls within the green region, the spinup is a forest. If it falls within the yellow 
-# # region, it is a prairie, and if it falls within the orange region then it is a savanna. 
-# pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/line_graph_treecover_v4.pdf")
-# ggplot(my_output,aes(x=year,y=Tree.BA_Fraction)) +
-#   annotate("rect",xmin=0, xmax=101, ymin=0.50, ymax=1, alpha=0.4, fill="green") + 
-#   annotate("rect", xmin=0, xmax=101, ymin=0.10, ymax=0.50, alpha=0.4, fill="orange") + 
-#   annotate("rect", xmin=0, xmax=101, ymin=0, ymax=0.10, alpha=0.4, fill="yellow") +
-#   annotate("text", x=20, y=0.55, label="Forest", size=2, alpha=0.9, family="Helvetica") + 
-#   annotate("text", x=25,y=0.15,label="Savanna",size=2,alpha=0.9,family="Helvetica") +
-#   annotate("text", x=20, y=0.05, label="Prairie", size=2, alpha=0.9, family="Helvetica") +
-#   # geom_hline(yintercept=0.10,color="#E69F00",size=0.5)+
-#   geom_line() +
-#   xlab("Years") +
-#   facet_wrap( ~ RUNID,ncol=5) + 
-#   ylab("Fractional Mature Tree Cover") + 
-#   ggtitle("Fractional Mature Tree Cover\nas a Proxy for Ecosystem State") +
-#   mytheme
-# dev.off() # ----
-# 
-# # GRAPH OF TREE BASAL AREA AS IT CHANGES THROUGH TIME
-# # ---------------------------------------------------
-# # Basal area was calculated from dbh, tree density, and patch area (I belive). This is measured in cm^2.  
-# pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/line_graph_ba_v4.pdf")
-# ggplot(my_output_general,aes(x=year,y=p.ba.tree)) +
-#   geom_line() +
-#   xlab("Years") +
-#   facet_wrap( ~ RUNID,ncol=5) + 
-#   ylab("Basal Area, cm^2") + 
-#   ggtitle("Basal Area Through Time") +
-#   mytheme
-# dev.off() # ------
-# 
-# # GRAPH OF THE TREE DENSITY AS IT CHANGES THROUGH TIME
-# # ----------------------------------------------------
-# # Tree density was given in the output, measured in plants m^-2. This graph shows density as it changes through time from the year
-# # 2200 until the year 2300. 
-# pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/line_graph_density_v4.pdf")
-# ggplot(my_output_general,aes(x=year,y=p.dens.tree)) +
-#   geom_line() +
-#   xlab("Years") +
-#   facet_wrap( ~ RUNID,ncol=5) + 
-#   ylab("Stem Density, plants m^-2") + 
-#   # ggtitle("Fractional Mature Tree Cover\nas a Proxy for Ecosystem State") +
-#   mytheme
-# dev.off() # -----
-# 
-# # GRAPH OF THE MAXIMUM DBH AS IT CHANGES THROUGH TIME
-# # ----------------------------------------------------
-# # The max DBH for each patch in each year was determined when the output was extracted into the tables. The maximum DBH is in cm and 
-# # generally increased through time. 
-# pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/line_graph_DBH_v4.pdf")
-# ggplot(my_output_general,aes(x=year,y=dbh.max)) +
-#   geom_line() +
-#   xlab("Years") +
-#   facet_wrap( ~ RUNID,ncol=5) + 
-#   ylab("Maximum DBH, cm") + 
-#   # ggtitle("Fractional Mature Tree Cover\nas a Proxy for Ecosystem State") +
-#   mytheme
-# dev.off() # ------
-# 
-# # SCATTER PLOT RELATING MATURE TREE COVER TO SOIL MOISTURE
-# # ---------------------------------------------------------
-# # Mature tree cover will vary with soil moisture. The tricky part is that fire frequency also varies with soil moisture.Here I took 
-# # the fractinal tree cover and  compared it to the soil composition (specifically the sand composition). This can be seen with all of 
-# # the variation, or it can be seen in the averages. To get a graph of the averages, simply uncomment the first ggplot line and comment
-# # out the second ggplot line. 
-# 
-# # Data table with average soil values # ------
-# output_var <- colnames(my_output_general)
-# output_var <- output_var[6:13]
-# for (VAR in output_var) {
-#   my_output_temp <- aggregate(my_output_general[,VAR],by=my_output_general[,c("pft","Soil")],FUN=mean, na.rm=T)
-#   colnames(my_output_temp) <- c("pft","Soil",VAR)
-#   if(VAR==output_var[1]){
-#     my_output_soil <- my_output_temp
-#   } else {
-#     my_output_soil <- merge(my_output_soil,my_output_temp)
-#   } #end if loop
-# }
-# my_output_temp <- aggregate(my_output[,"Tree.BA_Fraction"],by=list(my_output[,c("Soil")]), FUN=mean, na.rm=T)
-# colnames(my_output_temp) <- c("Soil","Tree.BA_Fraction")
-# my_output_soil <- merge(my_output_soil, my_output_temp)
-# rm(my_output_temp)
-# 
-# pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/scatterplot_soil_v4.pdf")
-# ggplot(my_output_soil,aes(x=Soil,y=Tree.BA_Fraction)) +
-# # ggplot(my_output,aes(x=Soil,y=Tree.BA_Fraction)) +
-#   geom_point() +
-#   annotate("rect",xmin=0, xmax=6, ymin=0.50, ymax=1, alpha=0.4, fill="green") + 
-#   annotate("rect", xmin=0, xmax=6, ymin=0.10, ymax=0.50, alpha=0.4, fill="orange") + 
-#   annotate("rect", xmin=0, xmax=6, ymin=0, ymax=0.10, alpha=0.4, fill="yellow") +
-#   xlab("Sand Compposition") +
-#   ylab("Mature Tree Cover") +
-#   mytheme
-# dev.off # ------
-# 
-# # SCATTER PLOT RELATING MAXIMUM DBH TO SOIL
-# # ------------------------------------------
-# pdf("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/DBH&soil_scatterplot.pdf")
-# ggplot(my_output_soil,aes(x=Soil,y=dbh.max)) +
-#   # ggplot(my_output,aes(x=Soil,y=dbh.max)) +
-#   geom_point() +
-#   xlab("Sand Compposition") +
-#   ylab("DBH, cm") +
-#   mytheme
-# dev.off # ------
-# 
-# # SCATTER PLOT RELATING MATURE TREE COVER TO FIRE
-# # -----------------------------------------------
-# # We can also relate fire to the mature tree cover. We can get an idea of what the correlation is, however it should be noted that 
-# # soil serves as a confounding variable, since the fire return interval tends to increase with increasing soil moisture. We can graph
-# # this in two ways: the first takes out variation in the dataset by only reporting the average tree cover values for each FRI. The 
-# # second way graphs all of the variation. There is always one point at the very edge of the graph; this represents the FRI denoted as 
-# # "Inf", which means that fire was never triggered. 
-# 
-# # Create data table of average values according to FRI #--------
-# output_var <- colnames(my_output_general)
-# output_var <- output_var[6:13]
-# for (VAR in output_var) {
-#   my_output_temp <- aggregate(my_output_general[,VAR],by=my_output_general[,c("pft","FRI")],FUN=mean, na.rm=T)
-#   colnames(my_output_temp) <- c("pft","FRI",VAR)
-#   if(VAR==output_var[1]){
-#     my_output_fire <- my_output_temp
-#   } else {
-#     my_output_fire <- merge(my_output_fire,my_output_temp)
-#   } #end if loop
-# }
-# my_output_temp <- aggregate(my_output[,"Tree.BA_Fraction"],by=list(my_output[,c("FRI")]), FUN=mean, na.rm=T)
-# colnames(my_output_temp) <- c("FRI","Tree.BA_Fraction")
-# my_output_fire <- merge(my_output_fire, my_output_temp)
-# rm(my_output_temp)
-# 
-# # SCATTER PLOT RELATING MATURE TREE COVER TO FIRE
-# #--------------------------------------------------------
-# pdf("../../Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/scatterplot_fri_v4.pdf")
-# ggplot(my_output_fire,aes(x=FRI,y=Tree.BA_Fraction)) + 
-# # ggplot(my_output, aes(x=FRI,y=Tree.BA_Fraction)) + 
-#   geom_point() +
-#   annotate("rect",xmin=0, xmax=35, ymin=0.50, ymax=1, alpha=0.4, fill="green") + 
-#   annotate("rect", xmin=0, xmax=35, ymin=0.10, ymax=0.50, alpha=0.4, fill="orange") + 
-#   annotate("rect", xmin=0, xmax=35, ymin=0, ymax=0.10, alpha=0.4, fill="yellow") +
-#   xlab("Fire Return Interval") +
-#   ylab("Mature Tree Cover") +
-#   mytheme
-# dev.off() #--------
-# 
-# # SCATTER PLOT RELATING MAXIMUM DBH TO FIRE
-# # -------------------------------------------
-# pdf ("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/DBH&FRI_scatterplot")
-# ggplot(my_output_general, aes(x=FRI,y=dbh.max)) + 
-#   geom_point() +
-#   xlab("Fire Return Interval") +
-#   ylab("Maximum DBH, cm") +
-#   mytheme
-# dev.off() #--------
-# 
-# # SCATTER PLOT RELATING TREE DENSITY TO FIRE
-# # -------------------------------------------
-# pdf ("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/Density&FRI_scatterplot")
-# # ggplot(my_output_general, aes(x=FRI,y=p.dens)) + 
-# ggplot(my_output_fire, aes(x=FRI,y=p.dens)) + 
-#   geom_point() +
-#   xlab("Fire Return Interval") +
-#   ylab("Tree Density, plans m^-2") +
-#   mytheme
-# dev.off() #--------
-# 
-# # SCATTER PLOT RELATING TREE DENSITY TO FIRE
-# # -------------------------------------------
-# pdf ("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/Density&FRI_scatterplot")
-# # ggplot(my_output_general, aes(x=FRI,y=p.dens)) + 
-# ggplot(my_output_fire, aes(x=FRI,y=p.dens)) + 
-#   geom_point() +
-#   xlab("Fire Return Interval") +
-#   ylab("Tree Density, plans m^-2") +
-#   mytheme
-# dev.off() #--------
-# 
-# # SCATTER PLOT RELATING BASAL AREA TO FIRE
-# # -------------------------------------------
-# pdf ("/Users/Cori/Research/Forests_on_the_Edge/URF 2018 Butkiewicz/Project_Output/v4/BA&FRI_scatterplot")
-# # ggplot(my_output_general, aes(x=FRI,y=p.ba)) + 
-# ggplot(my_output_fire, aes(x=FRI,y=p.ba)) + 
-#   geom_point() +
-#   xlab("Fire Return Interval") +
-#   ylab("Tree Density, plans m^-2") +
-#   mytheme
-# dev.off() #--------
-# 
+ggplot(dat.all.agg, aes(x=SLXSAND, y=FRI, fill=SM_FIRE)) + 
+  geom_bar()
